dc11953e86075f783f32835bb8692f3a0c2382cf
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2022-01-18 14:08:04 +0000

    feat: change button y padding

--- packages/react/src/button/Button.tsx
+++ packages/react/src/button/Button.tsx
@@ -22,7 +22,7 @@ const Button: FC<Props> = ({
   return (
     <button
       className={clsx(
-        'px-4 py-2 rounded-full uppercase border-2 transition-colors duration-100',
+        'px-4 py-6 rounded-full uppercase border-2 transition-colors duration-100',
         outline
           ? `hover:text-white text-${variant} border-${variant} ${borderActiveClasses[variant]}`
           : `text-white border-transparent bg-${variant}`,
